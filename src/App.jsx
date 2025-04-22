029f9ce697b7792cb82bdb64ba9b5a7d5424250b
Luis Galvan <luis_galvan221@hotmail.com>
2025-04-21 20:31:13 -0600

    add api key and api url

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>Hola</div>;
+  return (
+    <>
+      <AuthContextProvider>
+        <span>Hola Mundo</span>
+      </AuthContextProvider>
+    </>
+  );
 }
 
 export default App;
